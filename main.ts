4139b9e9b2855baf9b84321db10d8489a46738c7
DevKarpe <65222864+DevKarpe@users.noreply.github.com>
2020-05-13 10:43:31 +0100

    Removed the projectile. Made walls and buttons instead. Finished level 1, beginning level 2.

--- main.ts
+++ main.ts
@@ -54,6 +54,15 @@ function doNarrator () {
     pause(100)
     Narrator2.destroy()
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonTeal, function (sprite, location) {
+    tiles.setTileAt(location, sprites.dungeon.floorLight0)
+    for (let value of tiles.getTilesByType(sprites.dungeon.purpleSwitchDown)) {
+        tiles.setTileAt(value, sprites.dungeon.purpleSwitchUp)
+    }
+    for (let value of tiles.getTilesByType(sprites.dungeon.stairLadder)) {
+        tiles.setTileAt(value, sprites.dungeon.stairLarge)
+    }
+})
 function doPlayer () {
     Player1 = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -82,47 +91,84 @@ f b b b b f 2 2 2 2 f d 4 . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
     tiles.setTilemap(tiles.createTilemap(
-            hex`0a000d001401050505050505061414020f0f0f0f0f0f071414020f0f0f0f0f0f071414020f0f0f0f0f0f071414020f0f0f0f0f0f07141404090a0f0f0b090814141414020f0f0714141414010c0d0f0f0e05061414020f0f0f0f0f0f071414021212121212120714140212121212121207141402121212121213071414040909090909090814`,
+            hex`0a000d00140105050505050506141402130f0f0f0f0f071414020f0f0f0f0f0f071414020f0f0f0f0f0f071414020f0f0f0f0f0f07141404090a16160b0908141414140216160714141414010c0d16160e1c06141402170f0f0f0f0f0714140212121212121207141402121212121212071414021b121212120f071414040909090909090814`,
             img`
 2 2 2 2 2 2 2 2 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
-2 2 2 2 . . 2 2 2 2 
-2 2 2 2 . . 2 2 2 2 
-2 2 2 2 . . 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
 2 2 2 2 2 2 2 2 2 2 
 `,
-            [myTiles.tile0,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.floorLight4,sprites.dungeon.floorLight2,sprites.dungeon.purpleOuterWest1,sprites.dungeon.floorLight0,sprites.dungeon.buttonOrange,sprites.dungeon.hazardLava1],
+            [myTiles.tile0,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.floorLight4,sprites.dungeon.floorLight2,sprites.dungeon.purpleOuterWest1,sprites.dungeon.floorLight0,sprites.dungeon.buttonOrange,sprites.dungeon.hazardLava1,sprites.builtin.brick,sprites.dungeon.hazardSpike,sprites.dungeon.buttonTeal,sprites.dungeon.chestOpen,sprites.dungeon.collectibleInsignia,sprites.dungeon.stairLarge,sprites.dungeon.stairLadder,sprites.dungeon.purpleSwitchDown,sprites.dungeon.purpleSwitchUp],
             TileScale.Sixteen
         ))
     controller.moveSprite(Player1)
     scene.cameraFollowSprite(Player1)
 }
-let Player1: Sprite = null
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
+    tiles.setTileAt(location, sprites.dungeon.floorLight0)
+    for (let value of tiles.getTilesByType(sprites.dungeon.hazardSpike)) {
+        tiles.setTileAt(value, sprites.dungeon.floorLight0)
+        tiles.setWallAt(value, false)
+    }
+})
 let Narrator2: Sprite = null
-doPlayer()
-let enemyprojectile = sprites.create(img`
-. . . . . . . . . c c 8 . . . . 
-. . . . . . 8 c c c f 8 c c . . 
-. . . c c 8 8 f c a f f f c c . 
-. . c c c f f f c a a f f c c c 
-8 c c c f f f f c c a a c 8 c c 
-c c c b f f f 8 a c c a a a c c 
-c a a b b 8 a b c c c c c c c c 
-a f c a a b b a c c c c c f f c 
-a 8 f c a a c c a c a c f f f c 
-c a 8 a a c c c c a a f f f 8 a 
-. a c a a c f f a a b 8 f f c a 
-. . c c b a f f f a b b c c 6 c 
-. . . c b b a f f 6 6 a b 6 c . 
-. . . c c b b b 6 6 a c c c c . 
-. . . . c c a b b c c c . . . . 
-. . . . . c c c c c c . . . . . 
-`, SpriteKind.Projectile)
-enemyprojectile.setPosition(42, 47)
+let Player1: Sprite = null
+Player1 = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . . . . . . . . . . . 
+. . . f f f 2 2 f f f . . . . . . . . . . . . . 
+. . f f f 2 2 2 2 f f f . . . . . . . . . . . . 
+. f f f e e e e e e f f f . . . . . . . . . . . 
+. f f e 2 2 2 2 2 2 e e f . . . . . . . . . . . 
+. f e 2 f f f f f f 2 e f . . . . . . . . . . . 
+. f f f f e e e e f f f f . . . . . . . . . . . 
+f f e f b f 4 4 f b f e f f . . . . . . . . . . 
+f e e 4 1 f d d f 1 4 e e f . . . . . . . . . . 
+. f f f f d d d d d e e f . . . . . . . . . . . 
+f d d d d f 4 4 4 e e f . . . . . . . . . . . . 
+f b b b b f 2 2 2 2 f 4 e . . . . . . . . . . . 
+f b b b b f 2 2 2 2 f d 4 . . . . . . . . . . . 
+. f c c f 4 5 5 4 4 f 4 4 . . . . . . . . . . . 
+. . f f f f f f f f . . . . . . . . . . . . . . 
+. . . . f f . . f f . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(Player1)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0],
+            TileScale.Sixteen
+        ))
